3dc290a6e9b819a3fff75d4648174ae5d2812ce3
Nastyaa2 <105021314+Nastyaa2@users.noreply.github.com>
2022-05-14 13:00:07 +0300

    Add files via upload

---
+++ final task.cpp
@@ -0,0 +1,58 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+    struct stcount
+    {
+        int znach; 
+        int count = 0; 
+    };
+
+
+    int main()
+    {
+        int max = 10;
+        int a[10];
+
+        struct stcount counter[100] = { }; // счетчик
+
+
+        for (int i = 0; i < max; i++)
+        {
+            cout << "enter " << i + 1 << "-number:  " << endl;
+            cin >> a[i];
+
+            for (int x = 0; x < max; x++)
+
+                if (counter[x].count == 0) // если элемент еще не посчитан, добавим +1
+                {
+                    counter[x].znach = a[i];
+
+                    counter[x].count++;
+
+                    break;
+                }
+
+                else if (counter[x].znach == a[i]) // если элемент посчитан и значение == этому элементу
+                {
+                    counter[x].count++; // увеличим счетчик
+
+                    break;
+                }
+        }
+
+        cout << "------------------------------------" << endl;
+        cout << "duplicate items have been removed!" << endl;
+
+        for (int i = 0; i < max; i++)
+            if (counter[i].count == 1) {
+
+                cout << counter[i].znach << ",";
+            }
+
+
+
+
+    };
+
